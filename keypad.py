97ff90422529b68132fd9f736ac2bc2141badeb6
Dhruv Pande <dhruvpande4@gmail.com>
2022-01-27 10:53:37 -0500

    added allowed_machines

--- keypad.py
+++ keypad.py
@@ -7,10 +7,35 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
 
+import credentials as creds #file with db credentials
+import pymysql.cursors
+
+#Connecting to the Database
+connection = pymysql.connect(host=creds.dbhost,
+                             user=creds.dbuser,
+                             password=creds.dbpass,
+                             database=creds.dbname,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.Cursor)
+
+
+def allowed_machines(id): #returns list of machine IDs that a student can use
+    connection.ping(True)
+    with connection:
+        with connection.cursor() as cursor:
+            machs = []
+            cursor.execute("select mid from students_machines where sid="+id+" and can_use=1") 
+            result = cursor.fetchall()
+            if not result:
+                print("Invalid ID")
+                return -1
+            for l in result:
+                machs.append(l[0]) #being stored as ints
+            return machs
+
 class IDScreen(Screen):
     id_label = ObjectProperty() #make id text accessible here
-    def on_enter(self, *args):
-        self.id_label.text = 'ID: ' #reset id when coming back to this screen
+    def on_enter(self, *args): 
         return super().on_enter(*args)
 
     def updateID(self, key):
@@ -21,8 +46,16 @@ class IDScreen(Screen):
 
     def sendID(self):
         if (len(self.id_label.text) == 9):
-            #SEND ID TO SQL SERVER
-            self.manager.current = 'machine' #switch screen
+            id = self.id_label.text[4:]
+            # print(id)
+            allowed = allowed_machines(id) #SEND ID TO SQL SERVER and get list of machine ID's that student can use
+            if allowed != -1:
+                self.manager.current = 'machine' #switch screen
+            self.id_label.text = 'ID: '
+            
+            
+
+
 
 
 class MachineScreen(Screen):
