c69bba08202c4882e35e9da202e8b26ebcb621d2
22ghoshi <ishaanghosh10@gmail.com>
2022-02-10 10:55:12 -0500

    reformatted adminapp to kv

--- adminApp.py
+++ adminApp.py
@@ -3,6 +3,7 @@ from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 import credentials as creds #file with db credentials
 import pymysql.cursors
@@ -14,97 +15,27 @@ connection = pymysql.connect(host=creds.dbhost,
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.Cursor)
 
-class MyScreen(App):
+class AdminScreen(Screen):
+    def on_enter(self, *args):
+        return super().on_enter(*args)
+
+    def addStudent(self):
+        curr_id = self.ids.id.text
+        pass
+
+    def toggleStatus(self, id, currColor):
+        pass
+
+class LogScreen(Screen):
+    def on_enter(self, *args):
+        return super().on_enter(*args)
+
+class AdminApp(App):
     def build(self):
-        screen = FloatLayout()
-        self.txtInput = TextInput(
-            text = '',
-            size_hint = (0.3,0.05),
-            background_color = (1,0,1,0.5),
-            pos_hint= {'x':0.05, 'y':0.70},
-            font_size = '15sp')
-        self.lblTxt = Label(
-            text = 'Please Enter ID Numeber Below',
-            size_hint = (0.3,0.05),
-            pos_hint= {'x':0.05, 'y':0.75},
-            font_size = '15sp')
-        self.updateBtn = Button(
-            text ='Update', size_hint =(.15, .20),
-            background_color =(1,0,0,1),
-            pos_hint ={'x':0.80, 'y':0.65},
-            font_size='30sp')
-        self.addStudentBtn = Button(
-            text ='Add', size_hint =(.15, .20),
-            max_lines=2,
-            background_color =(1,0,0,1),
-            pos_hint ={'x':0.6, 'y':0.65 },
-            font_size='30sp',
-            on_press = self.addStudent)
-        self.machine1 = Button(
-            text ='1', size_hint =(.25, .25),
-            background_color =(1,0,0,1),
-            pos_hint ={'x':0, 'y':0 },
-            font_size='30sp',
-            on_press = self.press1)
-        self.machine2 = Button(
-            text ='2', size_hint =(.25, .25),
-            background_color =(1,0,0,1),
-            pos_hint ={'x':.25, 'y':0 },
-            font_size='30sp')
-        self.machine3 = Button(
-            text ='3', size_hint =(.25, .25),
-            background_color =(1,0,0),
-            pos_hint ={'x':.5, 'y':0 },
-            font_size='30sp')
-        machine4 = Button(
-            text ='4', size_hint =(.25, .25),
-            background_color =(1,0,0),
-            pos_hint ={'x':.75, 'y':0 },
-            font_size='30sp')
-
-        machine5 = Button(
-            text ='5', size_hint =(.25, .25),
-            background_color =(1,0,0),
-            pos_hint ={'x':0, 'y':.25 },
-            font_size='30sp')
-        machine6 = Button(
-            text ='6', size_hint =(.25, .25),
-            background_color =(1,0,0),
-            pos_hint ={'x':0.25, 'y':.25 },
-            font_size='30sp')
-        machine7 = Button(
-            text ='7', size_hint =(.25, .25),
-            background_color =(1,0,0),
-            pos_hint ={'x':0.5, 'y':.25 },
-            font_size='30sp')
-        machine8 = Button(
-            text ='8', size_hint =(.25, .25),
-            background_color =(1,0,0),
-            pos_hint ={'x':0.75, 'y':.25 },
-            font_size='30sp')
-
-        screen.add_widget(self.machine1)
-        screen.add_widget(self.machine2)
-        screen.add_widget(self.machine3)
-        screen.add_widget(machine4)
-        screen.add_widget(machine5)
-        screen.add_widget(machine6)
-        screen.add_widget(machine7)
-        screen.add_widget(machine8)
-        screen.add_widget(self.updateBtn)
-        screen.add_widget(self.addStudentBtn)
-        screen.add_widget(self.txtInput)
-        screen.add_widget(self.lblTxt)
-        return screen
-    def press1(self,obj):
-        if (self.machine1.background_color == [1,0,0,1]):
-            self.machine1.background_color = (0,2,0)
-        else:
-            self.machine1.background_color = (1,0,0,1)
-    def addStudent(self,obj):
-        if (self.machine1.background_color == [1,0,0,1]):
-            bool1 = False
-        else:
-            bool1 = True        
+        sm = ScreenManager()
+        sm.add_widget(AdminScreen(name = 'admin'))
+        sm.add_widget(LogScreen(name = 'log'))
+        return sm
+       
 if __name__ == "__main__":
-    MyScreen().run()
+    AdminApp().run()
